27a727625441cc668a5b4090b76be254ae0b8c0c
Wim Godden <wim@wimgodden.be>
2010-06-24 12:57:00 +0200

    Initial release of PHP53Compat CodeSniffer standard

---
+++ PHP53CompatCodingStandard.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * PHP53 PHP_CodeSniffer Coding Standard.
+ *
+ * PHP version 5.3
+ *
+ * @category  PHP
+ * @package   PHP53Compat
+ * @author    Wim Godden <wim.godden@cu.be>
+ * @copyright 2010 Cu.be Solutions bvba
+ * @license   http://www.linfo.org/bsdlicense.html BSD Licence
+ * @version   1.0
+ */
+
+if (class_exists('PHP_CodeSniffer_Standards_CodingStandard', true) === false) {
+    throw new PHP_CodeSniffer_Exception('Class PHP_CodeSniffer_Standards_CodingStandard not found');
+}
+
+if (version_compare(PHP_VERSION, '5.3.0', '<')) {
+    throw new PHP_CodeSniffer_Exception('PHP 5.3 is required to use the PHP53 codesniffer standard');
+}
+
+/**
+ * PHP53 PHP_CodeSniffer Coding Standard.
+ *
+ * @category  PHP
+ * @package   PHP53Compat
+ * @author    Wim Godden <wim.godden@cu.be>
+ * @copyright 2010 Cu.be Solutions bvba
+ * @license   http://www.linfo.org/bsdlicense.html BSD Licence
+ * @version   1.0
+ * @link      http://pear.php.net/package/PHP_CodeSniffer
+ */
+class PHP_CodeSniffer_Standards_PHP53_PHP53CodingStandard extends PHP_CodeSniffer_Standards_CodingStandard
+{
+
+
+    /**
+     * Return a list of external sniffs to include with this standard.
+     *
+     * This standard is limited to just the PHP 5.3 compatibility tests
+     *
+     * @return array
+     */
+    public function getIncludedSniffs()
+    {
+        return array();
+
+    }//end getIncludedSniffs()
+
+}//end class
